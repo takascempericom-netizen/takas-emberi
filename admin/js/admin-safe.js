353b91950c2e667737fbf40c79adb46dbcb66f77
takascempericom-netizen <uretenellercom@gmail.com>
2025-09-17 22:21:54 +0000

    feat(admin): siyah temalı sol menülü yeni yönetim paneli (İlan Onay/Mesajlar/Bildiri/Akan Yazı/Kullanıcılar/Şikayetler/Ayarlar + Çıkış)

--- admin/js/admin-safe.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('error', function(e){
-  var msg = String(e && (e.error || e.message) || '');
-  if (msg.indexOf('permission-denied') !== -1) {
-    console.warn('[admin] permission-denied (sönümlendi)');
-    e.preventDefault(); return false;
-  }
-});
--- admin/js/notify-bell.js
+++
@@ -1,9 +0,0 @@
-/** SAFE STUB (ES5) */
-(function (w) {
-  var api = {
-    init: function(){}, mount: function(){},
-    on: function(){}, off: function(){},
-    fetchPending: function(){ return Promise.resolve([]); }
-  };
-  w.NotifyBell = api; w.__notifyBell = api;
-})(window);
